8403b3fbd33062b853cf5abb5b839e3c4ab0ff3d
unknown <03emitul@VW1080.abbindustrigymnasium.local>
2013-01-14 13:35:27 +0100

    Removed debugmode

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def class_choice(classchoice=None, datechoice=None):
 		classchoice=classchoice, classList=classList)
 
 if __name__ == '__main__':
-	app.debug = True
+	app.debug = False
 	if app.debug == False:
 		port = int(os.environ.get('PORT', 5000))
 		app.run(host='0.0.0.0', port=port)
