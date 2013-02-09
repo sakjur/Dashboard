fb74a30ef151ead8bc8b53cc69944a1c7906fbf1
Emil Tullstedt <sakjur@gmail.com>
2013-02-09 12:00:55 +0100

    Broke everything. Complete refactoring TBD

--- app.py
+++ app.py
@@ -9,15 +9,11 @@ This project has no affliation with the school to do whatsoever.
 
 from flask import Flask, render_template
 import os
-import scraper
-import data
 
 APP = Flask(__name__)
-CLASSLIST = sorted(data.classList().keys())
 
 
 @APP.route('/')
-@APP.route('/class/')
 def class_list():
     """
     Output: HTML-formatted classlist and main page
@@ -25,20 +21,14 @@ def class_list():
     return render_template('classList.html', classList=CLASSLIST)
 
 
-@APP.route('/<int:classchoice>/')
-@APP.route('/<int:classchoice>/<int:datechoice>/')
-@APP.route('/class/<int:classchoice>/')
-@APP.route('/class/<int:classchoice>/<int:datechoice>/')
+@APP.route('/c/<int:classchoice>/')
+@APP.route('/c/<int:classchoice>/<int:datechoice>/')
 def class_choice(classchoice=None, datechoice=None):
     """
     Input: classchoice (4[0-9]) and datechoice (6-8[0-9])
     Output: HTML-formatted single day eventlist
     """
-    datechoice = str(datechoice)
-    choice = scraper.getClassSchedule(classchoice, datechoice)
-    return render_template('scheduleViewer.html', choice=choice,
-                           datechoice=datechoice, classchoice=classchoice,
-                           classList=CLASSLIST)
+    return "TBD" + str(classchoice) + str(datechoice)
 
 if __name__ == '__main__':
     APP.debug = True
--- static/js/script.js
+++
@@ -1,8 +0,0 @@
-/*
- * Dashboard JavaScript
- * by Emil Tullstedt
- * 2012-12-14
- */
-
-$(function() {
-});
